4d52d6a36802207fc1fa51399fb9628ca1689f82
Hugo AVRIL <hugoavril2910@gmail.com>
2023-04-11 17:27:12 +0200

    suppression des classes Observable et Observateur

--- src/main/java/com/observerexample/Observateur.java
+++
@@ -1,7 +0,0 @@
-package com.observerexample;
-
-public interface Observateur {
-        // Méthode appelée automatiquement lorsque l'état (position ou précision) du GPS
-        // change.
-        public void actualiser(Observable o);
-}
